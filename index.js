abe373c7c00b81ddce9d1a7b9732dc547b8e3afa
Yoshio Kanamori <y.k.kanamori@gmail.com>
2019-10-13 18:15:17 +0900

    t

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const middleware = require('./glc/middleware')
+const middleware = require('./glc/middleWare')
 
 app.use(bodyParser.urlencoded({
     extended: true
